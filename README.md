df8a8984c813649e672980178e95c3d411611752
kristensantee <mikeandkris27@gmail.com>
2022-07-07 14:52:07 -0700

    initial push, majority html, start js, start css

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# weather-dashboard
+# Weather Dashboard
+
+## Overview
+
+This module challenge was to create a weather outlook dashboard capable of retaining multiple city searches to plan a multi-city trip. The challenge focus is utilizing a third-party API to retrieve the data and localStorage for persistent data. 
+
+## Project Description
+
+The Acceptance Criteria for this project is as follows:
+
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+## Run and Install
+
+The deployed project can be accessed [here](). A screenshot for my weather dashboard can be found here:
+
+## How to Use the Project
+
+
+
+## How to Contribute to the Project
+
+This project was assigned to build familiarity with third-party APIs to build a functional weather dashboard and retain search history populated on the 
---
+++ assets/script.js
@@ -0,0 +1,67 @@
+//connect API
+//fetch API data: city, date, temp, wind, humidity, uv index, 5-day forecast of temp,wind,humidity, weather emoji
+
+var APIKey = "3d8192d986e0f88e98e2fa4d8b729817";
+var city = "Tacoma";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
+var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "units=imperial";
+var date = document.getElementById('date');
+// var currentDate = moment().format("YYYY");
+var searchButton = document.getElementById('search-btn');
+var cityInput = document.querySelector('.search-input');
+// console.log(currentDate);
+// var lat;
+// var long;
+
+//add event listener for search click
+searchButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    var searchInput = cityInput.value;
+    localStorage.setItem("city", searchInput);
+})
+
+fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        //fetch city name from search
+        var cityTempEl = document.getElementById('current-city-date');
+        cityTempEl.textContent = data.name;
+        //fetch current temperature, create, and append element
+        var currentTemp = document.createElement('span');
+        currentTemp.textContent = data.main.temp + "\u00B0";
+        document.getElementById("current-temp").appendChild(currentTemp);
+        //fetch current wind, create, and append element
+        var currentWind = document.createElement('span');
+        currentWind.textContent = data.wind.speed + "MPH";
+        document.getElementById("current-wind").appendChild(currentWind);
+        //fetch current humidity, create, and append element
+        var currentHumidity = document.createElement('span');
+        currentHumidity.textContent = data.main.humidity + "%";
+        document.getElementById('current-humidity').appendChild(currentHumidity);
+        var lat = data.coord.lat;
+        var long = data.coord.lon;
+        var latLonURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&appid=" + APIKey;
+        // var latLon = [lat, long];
+        return fetch(latLonURL)
+    })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                //fetch current UV index, create, and append element
+                var currentUV = document.createElement('span');
+                currentUV.textContent = data.current.uvi;
+                document.getElementById('current-uv').appendChild(currentUV);
+                //fetch current weather icon, create, and append element
+                var currentWeatherIcon = document.createElement('img');
+                currentWeatherIcon.setAttribute("src", data.current.weather[0].icon);
+                document.getElementById('current-city-date').appendChild(currentWeatherIcon);
+            })
+    
+fetch(forecastURL)
+  
+    
